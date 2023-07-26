5fa9daa715e796c61dbc93fec11d29c1d596b7f4
Molotvan <molotvan@yandex.ru>
2023-07-26 13:32:29 +0300

    second

--- src/Notebook.java
+++ src/Notebook.java
@@ -55,18 +55,18 @@ public class Notebook {
 
     @Override
     public String toString() {
-        return ("Название:  " + this.getName()+ "\n"+ "Модель "+ this.getModel() + "\n" + "Диагональ: " + this.getDiagonal()+ "\n" + "Память " + this.getHddValue() +
-                 "\n" + "Оперативная память " + this.getRamValue() + "\n" + "Процессор " + this.getProcessor() + "\n" + "Цена " + this.getCost());
+        return ("Название:  " + this.getName() + "\n" + "Модель " + this.getModel() + "\n" + "Диагональ: " + this.getDiagonal() + "\n" + "Память " + this.getHddValue() +
+                "\n" + "Оперативная память " + this.getRamValue() + "\n" + "Процессор " + this.getProcessor() + "\n" + "Цена " + this.getCost());
     }
 
-    protected boolean isSuit(Map<String, String> request){
-        if (this.getName().equals(request.get("Название"))||(request.get("Название")).equals("0")){
-            if(this.getModel().equals(request.get("Модель"))||(request.get("Модель")).equals("0")){
-                if(((request.get("Диагональ")).equals("0"))||(this.getDiagonal()==Integer.parseInt(request.get("Диагональ")))){
-                    if((this.getHddValue()>=Integer.parseInt(request.get("Объём HDD")))||((request.get("Объём HDD")).equals("0"))){
-                        if((this.getRamValue()>=Integer.parseInt(request.get("Оперативная память")))||((request.get("Оперативная память")).equals("0"))){
-                            if (this.getProcessor().equals(request.get("Процессор"))||(request.get("Процессор")).equals("0")){
-                                if (((Integer.parseInt(request.get("Цена, мин")))<=this.getCost() && this.getCost()<=(Integer.parseInt(request.get("Цена, макс"))))||((request.get("Цена, мин").equals("0"))||(request.get("Цена, макс").equals("0")))){
+    protected boolean isSuit(Map<String, String> request) {
+        if (this.getName().equals(request.get("Название")) || (request.get("Название")).equals("0")) {
+            if (this.getModel().equals(request.get("Модель")) || (request.get("Модель")).equals("0")) {
+                if (((request.get("Диагональ")).equals("0")) || (this.getDiagonal() == Integer.parseInt(request.get("Диагональ")))) {
+                    if ((this.getHddValue() >= Integer.parseInt(request.get("Объём HDD"))) || ((request.get("Объём HDD")).equals("0"))) {
+                        if ((this.getRamValue() >= Integer.parseInt(request.get("Оперативная память"))) || ((request.get("Оперативная память")).equals("0"))) {
+                            if (this.getProcessor().equals(request.get("Процессор")) || (request.get("Процессор")).equals("0")) {
+                                if (((Integer.parseInt(request.get("Цена, мин"))) <= this.getCost() && this.getCost() <= (Integer.parseInt(request.get("Цена, макс")))) || ((request.get("Цена, мин").equals("0")) || (request.get("Цена, макс").equals("0")))) {
                                     return true;
                                 }
                             }
